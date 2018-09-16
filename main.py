4abcbc237f0fe5822545da4d273e120a9cbcdb13
Raymond Blum <rsygeeknyc@gmail.com>
2018-09-16 12:55:06 -0400

    Split requests for delay and target as they have different lifetimes

--- main.py
+++ main.py
@@ -1,28 +1,31 @@
 import webapp2
 from models import _Timestamp, _Delay
 
-class SetHandler(webapp2.RequestHandler):
+class SetTargetHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Setting!')
+        self.response.write('Setting target!')
         params = self.request.get_all('datetime')
         if params:
             timestamp = long(params[0])
             print 'saving {}'.format(timestamp)
             _Timestamp.Save(timestamp)
 
-class GetHandler(webapp2.RequestHandler):
+class GetTargetHandler(webapp2.RequestHandler):
     def get(self):
         timestamp = _Timestamp.Get()
         if timestamp:
             print 'got timestamp: {}'.format(timestamp)
             self.response.write('datetime={}\n'.format(timestamp.datetime))
 
+class GetDelayHandler(webapp2.RequestHandler):
+    def get(self):
         delay = _Delay.Get()
         if delay:
             print 'got delay: {}'.format(delay)
             self.response.write('delay={}\n'.format(delay.seconds))
 
 app = webapp2.WSGIApplication([
-    ('/settarget', SetHandler),
-    ('/gettarget', GetHandler)
+    ('/settarget', SetTargetHandler),
+    ('/gettarget', GetTargetHandler),
+    ('/getdelay', GetDelayHandler)
 ], debug=True)
