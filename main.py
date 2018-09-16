a41785c3889bce70f3a790627c87e61e93a6c80c
Raymond Blum <rsygeeknyc@gmail.com>
2018-09-16 12:40:18 -0400

    Add delay seconds to Ziggy Add default timestamp

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import webapp2
-from models import _Timestamp
+from models import _Timestamp, _Delay
 
 class SetHandler(webapp2.RequestHandler):
     def get(self):
@@ -15,10 +15,14 @@ class GetHandler(webapp2.RequestHandler):
         timestamp = _Timestamp.Get()
         if timestamp:
             print 'got timestamp: {}'.format(timestamp)
-            self.response.write('datetime={}'.format(timestamp.datetime))
+            self.response.write('datetime={}\n'.format(timestamp.datetime))
+
+        delay = _Delay.Get()
+        if delay:
+            print 'got delay: {}'.format(delay)
+            self.response.write('delay={}\n'.format(delay.seconds))
 
 app = webapp2.WSGIApplication([
     ('/settarget', SetHandler),
     ('/gettarget', GetHandler)
 ], debug=True)
-
--- models.py
+++ models.py
@@ -1,6 +1,9 @@
 import sys
 from google.appengine.ext import ndb
 
+DEFAULT_DELAY_SECONDS = 10
+DEFAULT_TIMESTAMP = 1520985600000  # Midnight, 3/14/2018 
+
 class _Timestamp(ndb.Model):
   datetime = ndb.IntegerProperty()
 
@@ -40,3 +43,46 @@ class _Timestamp(ndb.Model):
     persisted_value = _Timestamp(datetime=timestamp)
     key = persisted_value.put()
     print("Added {}".format(key))
+
+class _Delay(ndb.Model):
+  seconds = ndb.IntegerProperty()
+
+  @classmethod
+  def Delete(cls):
+    print("Deleting {}".format(cls.__name__))
+    all_instances = cls.query()
+    instance_count = 0
+    for key in all_instances.iter(keys_only=True):
+      print("Deleting {}".format(key))
+      key.delete()
+      instance_count += 1
+    print("Deleted {} of {}".format(instance_count, cls.__name__))
+ 
+  @classmethod
+  def Get(cls):
+    print("Finding all {}".format(cls.__name__))
+    instance_query = cls.query()
+    instances = instance_query.iter()
+    entities = []
+    for instance in instances:
+      entities.append(instance)
+      print("found: {}".format(instance))
+    print("Found {} instances".format(len(entities)))
+    if entities:
+      return entities[0]
+    else:
+      return None
+
+  @classmethod
+  def Save(cls, delay):
+    cls.Delete()
+    cls.Add(delay)
+
+  @classmethod
+  def Add(cls, delay):
+    persisted_value = _Delay(seconds=delay)
+    key = persisted_value.put()
+    print("Added {}".format(key))
+
+_Delay.Save(DEFAULT_DELAY_SECONDS)
+_Timestamp.Save(DEFAULT_TIMESTAMP)
